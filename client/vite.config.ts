01cf480613772c10acea25a74ab2bc4828d59f09
Gal Dagan <gald2345@gmail.com>
2024-07-30 17:20:35 +0300

    Setup frontend with initial project structure and dependencies

---
+++ client/vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './', 
+  plugins: [react()],
+  test: {
+    setupFiles: ['./src/tests/setupTests.ts'],
+    globals: true,
+    environment: 'jsdom',
+  },
+});
